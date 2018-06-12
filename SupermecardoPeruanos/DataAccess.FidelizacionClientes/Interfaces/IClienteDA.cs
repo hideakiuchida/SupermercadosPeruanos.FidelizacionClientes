d68b67b91ade8146a47f10e0c2491d45cabf56ab
alexloaiza1290 <38789994+alexloaiza1290@users.noreply.github.com>
2018-06-12 02:39:30 -0500

    Creacion de la interfaz cliente

--- SupermecardoPeruanos/DataAccess.FidelizacionClientes/Interfaces/IClienteDA.cs
+++ SupermecardoPeruanos/DataAccess.FidelizacionClientes/Interfaces/IClienteDA.cs
@@ -5,5 +5,8 @@ namespace DataAccess.FidelizacionClientes.Interfaces
     public interface IClienteDA
     {
         Cliente GetCliente(string numeroDocumento);
+        void InsertCliente(Cliente cliente);
+        void UpdateCliente(Cliente cliente);
+        void DeleteCliente(int codigo);
     }
 }
