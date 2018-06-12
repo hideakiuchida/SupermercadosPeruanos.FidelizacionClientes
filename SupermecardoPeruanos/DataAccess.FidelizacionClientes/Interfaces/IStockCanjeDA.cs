28b6b7d5a2bc39a6a6f82dd7aa3053afb25884b6
alexloaiza1290 <38789994+alexloaiza1290@users.noreply.github.com>
2018-06-12 02:25:45 -0500

    alex
    Pruebas de conexion

---
+++ SupermecardoPeruanos/DataAccess.FidelizacionClientes/Interfaces/IStockCanjeDA.cs
@@ -0,0 +1,12 @@
+﻿using Model.FidelizacionClientes;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DataAccess.FidelizacionClientes.Interfaces
+{
+    interface IStockCanjeDA
+    {
+        StockCanje GetByProducto(int codigoProducto);
+    }
+}
---
+++ SupermecardoPeruanos/Model.FidelizacionClientes/StockCanje.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Model.FidelizacionClientes
+{
+    public class StockCanje
+    {
+        public int Codigo { get; set; }
+        public double Stock { get; set; }
+        public DateTime FechaRegistro { get; set; }
+        public Producto Producto { get; set; }
+    }
+}
