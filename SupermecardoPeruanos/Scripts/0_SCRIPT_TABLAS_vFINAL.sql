cad92e555334905f95d5d7682dbdbce427869f95
hideakiuchida <alonso.uchida@gmail.com>
2018-06-14 10:30:28 -0500

    Adding MySql Scripts
    Adding MySql Scripts

---
+++ SupermecardoPeruanos/Scripts/0_SCRIPT_TABLAS_vFINAL.sql
@@ -0,0 +1,174 @@
+use sigesu;
+
+drop table if exists historial_compra;
+drop table if exists infocorp;
+drop table if exists stock_canje;
+drop table if exists solicitud_afiliacion;
+drop table if exists tarjetaoh;
+drop table if exists calificacion_cliente;
+drop table if exists cliente_afiliado;
+drop table if exists catalogo_canje;
+drop table if exists producto_canje;
+drop table if exists categoria_canje;
+
+create table cliente_afiliado (
+  `id` int(11) not null auto_increment comment 'ID de la Tabla', 
+  `nombre_completo` varchar(50) default null  comment 'Nombre Completo de Cliente',
+  `apellido_paterno` varchar(50) default null  comment 'Apellido Paterno de Cliente',
+  `apellido_materno` varchar(50) default null  comment 'Apellido Materno de Cliente',
+  `tipo_documento` varchar(3) default null comment 'Tipo Documento de Identidad (DNI, RUC)',
+  `numero_documento` varchar(10) default null comment 'Número  de Documento de Identidad',
+  `fecha_nacimiento` datetime default null comment 'Fecha de Nacimiento de Cliente',
+  `sexo` varchar(1) default null comment 'Sexo de Cliente',
+  `email` varchar(50) default null comment 'Correo de Cliente',
+  `direccion` varchar(100) default null comment 'Dirección de Cliente',
+  `telefono_fijo` varchar(20) default null comment 'Teléfono fijo de Cliente',
+  `telefono_movil` varchar(20) default null comment 'Teléfono móvil de Cliente',
+  `situacion_laboral` varchar(20) default null comment 'Situacion laboral (independiente, empleado)',
+  `estado_cliente` varchar(20) default null comment 'Status (activo, inactivo)',
+  `indicador_tarjeta` varchar(1) default 'N' comment 'Status si tiene tarjeta oh (s/n)',
+  `tarjeta_vclub` varchar(1) default 'N' comment 'Status si tiene tarjeta vea club (s/n)',
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint cliente_afiliado_pk primary key (id)
+) engine=innodb default charset=utf8 comment='Maestro de Clientes afiliados';
+
+create table categoria_canje (
+  `id` int(11) not null auto_increment  comment 'ID de la Tabla', 
+  `nombre_categoria` varchar(50) default null comment 'Descripcion de Categoria',
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint categoria_canje_pk primary key (id)
+) engine=innodb default charset=utf8  comment='Maestro de Categoría de Productos para Canje';
+
+create table producto_canje (
+  `id` int(11) not null auto_increment comment 'ID de la Tabla',
+  `nom_prod_canj` varchar(50) default null comment 'Nombre del Producto para Canje',
+  `id_categoria_producto` int(11) default null  comment 'ID Categoria Producto',
+  `valor` decimal(16,4) default null comment 'Valor del canje de producto',
+  `tipo_canje` int(11) default null comment ' Tipo de Canje (1: Producto, 0: Servicio)',
+  `descripcion_producto` varchar(500) default null comment 'Descripcion detallada  del producto', 
+  `condicion_canje` varchar(500) default null comment 'Condicion para canje producto',
+  `dir_ruta` varchar(100) default null comment 'Ruta imagen producto',
+  `img_prod` longblob comment 'imagen producto',
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint producto_canje_pk primary key (id),
+  constraint producto_canje_fk foreign key (id_categoria_producto) 
+        references categoria_canje (id)
+) engine=innodb default charset=utf8 comment='Maestro de Productos para Canje';
+
+create table catalogo_canje ( 
+  `id` int(11) not null auto_increment comment 'ID de la Tabla',
+  `id_catalogo` int(11) not null comment 'ID Catalogo Canje',
+  `id_producto_canje` int(11) not null comment 'ID Producto Canje',
+  `id_categoria_canje` int(11) not null comment 'ID Categoria Canje',
+  `fecha_inicial_catalogo` datetime default null,
+  `fecha_final_catalogo` datetime default null,
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint catalogo_canje_pk primary key (id),
+  constraint catalogo_canje_uk unique (id_catalogo, id_producto_canje, id_categoria_canje),
+  constraint catalogo_canje_fk foreign key (id_producto_canje) 
+        references producto_canje (id),
+  constraint catalogo_canje_fk2 foreign key (id_categoria_canje) 
+        references categoria_canje (id)	
+) engine=innodb default charset=utf8 comment='Maestro de Catálogo de Productos para Canje';
+
+create table historial_compra (
+  `id` int(11) not null auto_increment comment 'ID de la Tabla',
+  `id_cliente_historial` int(11) not null comment 'ID Cliente para su historial de compras',
+  `id_producto_historial` int(11) not null comment 'ID Producto Canje',
+  `id_categoria_historial` int(11) not null comment 'ID Categoria Canje',
+  `importe_compra` decimal(16,4) default null comment 'Importe  de  compra',
+  `fecha_compra` datetime not null comment 'Fecha de compra',
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint historial_compra_pk primary key (id),
+  constraint historial_compra_uk unique (id_cliente_historial, id_producto_historial, id_categoria_historial),
+  constraint historial_compra_fk foreign key (id_cliente_historial) 
+        references cliente_afiliado (id),
+  constraint historial_compra_fk1 foreign key (id_producto_historial) 
+        references producto_canje (id)	,
+  constraint historial_compra_fk2 foreign key (id_categoria_historial) 
+        references categoria_canje (id)		
+) engine=innodb default charset=utf8 comment='Historial de Compras';
+
+create table infocorp ( 
+  `id` int(11) not null auto_increment comment 'ID de la Tabla',
+  `entidad_financiera` varchar(50) default null comment 'Nombre de la Entidad Financiera',
+  `importe_deuda` decimal(16,4) default null comment 'Importe deuda Infocorp',
+  `calificacion_sbs` int(11) default null comment 'Calificacion SBS (0,1,2,3,4)',
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint infocorp_pk primary key (id)
+) engine=innodb default charset=utf8 comment='Información de Infocorp';
+
+create table solicitud_afiliacion (
+  `id` int(11) not null auto_increment comment 'ID de la Tabla',
+  `estado_solicitud` varchar(20) default null comment 'Estado de la Solicitud (Atentido, Activo)',
+  `fecha_solicitud` datetime default null  comment 'Fecha de la Solicitud',
+  `codigo_solicitante` varchar(10) not null comment 'DNI del solicitante (futuro cliente)',
+  `nombre_solicitante` varchar(50) default null comment 'Nombre de Solicitante',
+  `paterno_solicitante` varchar(50) default null comment 'Apellido paterno solicitante',
+  `materno_solicitante` varchar(50) default null comment 'Apellido materno solicitante',
+  `tipo_identidad` varchar(3) default null comment 'Tipo documento identidad (DNI, CEX)',
+  `numero_identidad` varchar(10) default null comment 'Número documento identidad',
+  `email_solicitante` varchar(50) default null comment 'Correo electronico del solicitante',
+  `telefono_solicitante` varchar(20) default null comment 'Teléfono del solicitante',
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint solicitud_afiliacion_pk primary key (id)
+) engine=innodb default charset=utf8  comment='Solicitud de Afiliaciones';
+
+create table stock_canje (
+  `id` int(11) not null auto_increment comment 'ID de la Tabla',
+  `id_stock_categoria` int(11) not null comment 'ID  de Categoria Canje',
+  `id_stock_producto` int(11) not null comment 'ID  de Producto Canje',
+  `stock` decimal(16,4) not null comment 'Cantidad de Stock de Productos para Canje',
+  `fecha_registro` datetime default null comment 'Fecha de Registro Stock',
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint stock_canje_pk primary key (id),
+  constraint stock_canje_uk unique (id_stock_categoria, id_stock_producto),
+  CONSTRAINT stock_canje_fk1 FOREIGN KEY (`id_stock_categoria`) REFERENCES categoria_canje (id),
+  CONSTRAINT stock_canje_fk2 FOREIGN KEY (`id_stock_producto`) REFERENCES producto_canje (id)
+) engine=innodb default charset=utf8 comment='Cantidad de Stock de Productos para Canje';
+
+create table tarjetaoh (
+  `id` int(11) not null auto_increment comment 'ID de la Tabla',
+  `id_tarjeta_cliente` int(11)  null comment 'ID de Cliente',
+  `tipo_tarjeta` varchar(20) default null comment 'Tipo de tarjeta (Gold, Normal)',
+  `numero_tarjeta` bigint(20) default null comment 'Número de tarjeta',
+  `bin_tarjeta` varchar(10) default null comment 'Código Bin de tarjeta',
+  `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+  `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+  `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+  constraint tarjetaoh_pk primary key (id),
+  constraint tarjetaoh_fk foreign key (id_tarjeta_cliente) 
+        references cliente_afiliado (id)
+) engine=innodb default charset=utf8 comment='Tarjetas Oh';
+
+create table calificacion_cliente (
+   `id` int not null auto_increment comment 'ID de la Tabla',
+   `id_cliente` int(11) not null comment 'ID de Cliente',
+   `calificacion_crediticia` varchar(20) null comment 'Calificación Crediticia del Cliente',
+   `linea_credito` decimal(16,4) null comment 'Línea de Crédito del Cliente',
+   `sueldo_cliente` decimal(16,4) null  comment 'Sueldo del Cliente',
+   `otros_ingresos_cliente` decimal(16,4) null comment 'Otro Ingreso del Cliente',
+   `estado_calificacion_cliente` int null  comment 'Estado de Calificación Crediticia del Cliente',
+   `fecha_grabacion` datetime not null default now() comment 'Fecha de grabación (auditoria)',
+   `usuario_grabacion` varchar(50) not null default 'system' comment 'Usuario (username) de grabación (auditoria)',
+   `ventana_grabacion` varchar(15) not null default 'system' comment 'Nemónico ventana de grabación (auditoria)',
+    constraint calificacion_cliente_pk primary key (id),
+	constraint calificacion_cliente_fk foreign key (id_cliente) 
+        references cliente_afiliado (id)
+) engine=innodb default charset=utf8  comment='Calificación del Cliente';
---
+++ SupermecardoPeruanos/Scripts/2_SCRIPT_STORES vFINAL.sql
@@ -0,0 +1,315 @@
+USE SIGESU;
+
+DROP PROCEDURE IF EXISTS CalificacionCliente_Q02; 
+delimiter &&
+CREATE Procedure CalificacionCliente_Q02 (IN P_ID_Cliente INTEGER)
+-- ===============================================================  
+-- Propósito: Buscar Calificacion de Cliente
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_cliente 		-> Código de Cliente
+-- ===============================================================   
+Begin
+	If (P_ID_Cliente = 0) Then
+		Select id, id_cliente, calificacion_crediticia, linea_credito,
+		sueldo_cliente, otros_ingresos_cliente, estado_calificacion_cliente, usuario_grabacion, 
+		fecha_grabacion, ventana_grabacion
+		From calificacion_cliente;
+	Else 
+		Select id, id_cliente, calificacion_crediticia, linea_credito,
+		sueldo_cliente, otros_ingresos_cliente, estado_calificacion_cliente, usuario_grabacion, 
+		fecha_grabacion, ventana_grabacion
+		From calificacion_cliente
+		Where id_cliente = p_id_cliente;
+	End If;
+End &&
+delimiter ;
+-- CALL CALIFICACIONCLIENTE_Q02 (0);
+
+DROP PROCEDURE IF EXISTS Categoria_Q01; 
+delimiter &&
+CREATE PROCEDURE Categoria_Q01()
+-- ===============================================================  
+-- Propósito: Cargar Categorias
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+-- ===============================================================  
+Begin
+	Select id, nombre_categoria
+	From  categoria_canje;
+End &&
+delimiter ;
+--  CALL Categoria_Q01;
+
+DROP PROCEDURE IF EXISTS Cliente_Q01; 
+delimiter &&
+CREATE PROCEDURE Cliente_Q01 (IN P_Docu_Iden INTEGER)
+-- ===============================================================  
+-- Propósito: Buscar Datos de Cliente
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_docu_iden 		-> Número de Documento de Identidad
+-- ===============================================================  
+Begin
+	Select id, nombre_completo, apellido_paterno, apellido_materno,
+	tipo_documento, numero_documento, fecha_nacimiento, sexo,
+	email, direccion, telefono_fijo, telefono_movil,
+	situacion_laboral, estado_cliente, indicador_tarjeta, tarjeta_vclub
+	From cliente_afiliado
+    Where numero_documento Like CONCAT('%', P_Docu_Iden, '%');
+End &&
+delimiter ;
+-- CALL Cliente_Q01(45);
+
+DROP PROCEDURE IF EXISTS Historial_Compra_Q01; 
+delimiter &&
+CREATE PROCEDURE Historial_Compra_Q01 (IN P_Id_Cliente INTEGER)
+-- ===============================================================  
+-- Propósito: Buscar Historial de Compras
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_cliente 		-> Código de Cliente
+-- ===============================================================   
+Begin
+	If (P_Id_Cliente = 0) Then
+		Select id_cliente_historial, id_producto_historial, id_categoria_historial, importe_compra, fecha_compra
+	    From   historial_compra;
+	ELSE
+		Select id_cliente_historial, id_producto_historial, id_categoria_historial, importe_compra, fecha_compra
+	    From   historial_compra
+		Where  id = P_Id_Cliente;
+	End If;
+End &&
+delimiter ;
+--  CALL Historial_Compra_Q01(0);
+
+DROP PROCEDURE IF EXISTS Infocorp_Q03;
+delimiter &&
+CREATE Procedure Infocorp_Q03 (IN P_Id_Cliente INTEGER)
+-- ===============================================================  
+-- Propósito: Buscar Datos de Infocorp
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_cliente 		-> Código de Cliente
+-- ===============================================================   
+Begin
+	If (P_Id_Cliente = 0) Then
+		Select id, entidad_financiera, importe_deuda, calificacion_sbs
+		From infocorp;
+	Else
+	    Select id, entidad_financiera, importe_deuda, calificacion_sbs
+		From infocorp
+		Where id = P_Id_Cliente;
+	END IF;
+END &&
+delimiter ;
+-- CALL Infocorp_Q03(4);
+
+DROP PROCEDURE IF EXISTS Producto_Q01;
+delimiter &&
+CREATE Procedure Producto_Q01 (IN P_Id_Producto INTEGER)
+-- ===============================================================  
+-- Propósito: Buscar Datos de Producto
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_producto 		-> Código de Producto
+-- ===============================================================   
+Begin
+	If (P_Id_Producto = 0) Then
+		Select t2.id 'id_producto', t2.nom_prod_canj, t2.descripcion_producto, t2.dir_ruta 'imagen', 
+		t2.valor, t1.stock, t2.condicion_canje -- t2.id_categoria_producto 'codigo categoria', t3.nombre_categoria 'desc. categoria'
+		From stock_canje t1 inner join producto_canje t2 on
+		( t2.id = t1.id and t2.id_categoria_producto = t1.id_stock_categoria )
+		inner join categoria_canje t3 on
+		( t3.id = t2.id_categoria_producto )
+		Order by t1.id_stock_categoria;
+	Else
+		Select t2.id 'id_producto', t2.nom_prod_canj, t2.descripcion_producto, t2.dir_ruta 'imagen', 
+		t2.valor, t1.stock, t2.condicion_canje -- t2.id_categoria_producto 'codigo categoria', t3.nombre_categoria 'desc. categoria'
+		From stock_canje t1 inner join producto_canje t2 on
+		( t2.id = t1.id and t2.id_categoria_producto = t1.id_stock_categoria )
+		inner join categoria_canje t3 on
+		( t3.id = t2.id_categoria_producto )
+		Where t1.id = P_Id_Producto
+		Order By t1.id_stock_categoria;
+	End If;
+End&&
+delimiter ;
+--  CALL P_Id_Producto(5); 
+
+DROP PROCEDURE IF EXISTS Productos_x_Catalogo_Q01;
+delimiter &&
+CREATE Procedure Productos_x_Catalogo_Q01 (IN P_Id_Catalogo INTEGER)
+-- ===============================================================  
+-- Propósito: Buscar Catalogos de Producto
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_catalogo 		-> Código de Catálogo
+-- ===============================================================   
+Begin
+	If (P_Id_Catalogo = 0) Then
+		Select t1.id_producto_canje 'codigo producto canje', t2.nom_prod_canj 'desc. producto canje', t1.id_categoria_canje 'codigo categoria', t3.nombre_categoria 'desc. categoria',
+		t2.valor 'puntos canje', case when t2.tipo_canje = 1 then 'producto' else 'servicio' end 'tipo producto', t2.condicion_canje 'condiciones producto canje'
+		from catalogo_canje t1 inner join producto_canje t2 on
+		( t2.id = t1.id_producto_canje and t2.id_categoria_producto = t1.id_categoria_canje )
+		inner join categoria_canje t3 on
+		( t3.id = t2.id_categoria_producto )
+		-- Where month(t1.fecha_inicial_catalogo) = month (current_timestamp) or month(t1.fecha_final_catalogo) = month (current_timestamp)
+		Order by t1.id_categoria_canje, t1.id_producto_canje;
+	Else
+        Select t1.id_producto_canje 'codigo producto canje', t2.nom_prod_canj 'desc. producto canje', t1.id_categoria_canje 'codigo categoria', t3.nombre_categoria 'desc. categoria',
+		t2.valor 'puntos canje', case when t2.tipo_canje = 1 then 'producto' else 'servicio' end 'tipo producto', t2.condicion_canje 'condiciones producto canje'
+		from catalogo_canje t1 inner join producto_canje t2 on
+		( t2.id = t1.id_producto_canje and t2.id_categoria_producto = t1.id_categoria_canje )
+		inner join categoria_canje t3 on
+		( t3.id = t2.id_categoria_producto )
+		Where t1.id = P_Id_Catalogo
+		Order by t1.id_categoria_canje, t1.id_producto_canje;
+	End If;
+End&&
+delimiter ;
+--  CALL PRODUCTOS_X_CATALOGO_Q01(1); 
+
+DROP PROCEDURE IF EXISTS Stock_x_Producto_Q01;
+delimiter &&
+CREATE Procedure Stock_x_Producto_Q01 (IN P_Id_Producto INTEGER)
+-- ===============================================================  
+-- Propósito: Buscar Datos de Producto
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_producto 		-> Código de Producto
+-- ===============================================================   
+Begin
+	If (P_Id_Producto = 0) Then 
+		Select t2.id 'codigo producto canje', t2.dir_ruta 'imagen', t2.valor 'puntos canje', t1.stock 'stock',
+		t2.nom_prod_canj 'desc. producto canje', t2.id_categoria_producto 'codigo categoria', t3.nombre_categoria 'desc. categoria'
+		From stock_canje t1 inner join producto_canje t2 on
+		( t2.id = t1.id_stock_producto and t2.id_categoria_producto = t1.id_stock_categoria )
+		Inner join categoria_canje t3 on
+		( t3.id = t2.id_categoria_producto )
+		Order by t1.id_stock_producto;
+	Else
+		Select t2.id 'codigo producto canje', t2.dir_ruta 'imagen', t2.valor 'puntos canje', t1.stock 'stock',
+		t2.nom_prod_canj 'desc. producto canje', t2.id_categoria_producto 'codigo categoria', t3.nombre_categoria 'desc. categoria'
+		From stock_canje t1 inner join producto_canje t2 on
+		( t2.id = t1.id_stock_producto and t2.id_categoria_producto = t1.id_stock_categoria )
+		Inner join categoria_canje t3 on
+		( t3.id = t2.id_categoria_producto )
+		Where t1.id = P_Id_Producto
+		Order By t1.id_stock_producto;
+	End If;
+End&&
+delimiter ;
+-- CALL Stock_x_Producto_Q01(5); 
+
+DROP PROCEDURE IF EXISTS TarjetaOh_I01;
+delimiter &&
+CREATE Procedure TarjetaOh_I01 (IN P_Id_Tarjeta INTEGER, IN P_Id_Cliente INTEGER, IN P_Tipo_Tarjeta VARCHAR(20), IN P_Num_Tarjeta INTEGER, IN P_Bin_Tarjeta VARCHAR(10))
+-- ===============================================================  
+-- Propósito: Registrar Tarjeta OH
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_tarjeta 		-> Código de Tarjeta Oh
+--    p_id_cliente      -> Código de Cliente
+--    p_tipo_tarjeta    -> Tipo de Tarjeta
+--    p_num_tarjeta     -> Número de Tarjeta
+--    p_bin_tarjeta     -> Bin de Tarjeta
+-- ===============================================================   
+Begin
+	Insert into tarjetaoh (id, id_tarjeta_cliente, tipo_tarjeta, numero_tarjeta,
+		bin_tarjeta, usuario_grabacion, fecha_grabacion, ventana_grabacion)
+	Values
+	   (p_id_tarjeta, p_id_cliente, p_tipo_tarjeta ,p_num_tarjeta, p_bin_tarjeta,'admin', current_timestamp,'System');
+End&&
+delimiter ;
+
+DROP PROCEDURE IF EXISTS TarjetaOh_Q01;
+delimiter &&
+CREATE PROCEDURE TarjetaOh_Q01 (IN P_Id_Cliente INTEGER)
+-- ===============================================================  
+-- Propósito: Buscar Datos de Tarjeta Oh
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_cliente 		-> Código de Cliente
+-- ===============================================================    
+Begin
+	If (P_Id_Cliente = 0) Then
+		Select id, id_tarjeta_cliente, tipo_tarjeta, numero_tarjeta,
+		bin_tarjeta
+		From tarjetaoh;
+	Else
+	    Select id, id_tarjeta_cliente, tipo_tarjeta, numero_tarjeta,
+		bin_tarjeta
+		From tarjetaoh
+		Where id_tarjeta_cliente = P_Id_Cliente;
+	End If;
+End&&
+delimiter ;
+-- CALL TarjetaOh_Q01 (1);
+
+DROP PROCEDURE IF EXISTS TarjetaOh_Solicitud_I02;
+delimiter &&
+CREATE PROCEDURE TarjetaOh_Solicitud_I02 (IN P_Id_Cliente INTEGER, IN P_Tipo_Tarjeta VARCHAR(20), IN P_Num_Tarjeta BIGINT(20))
+-- ===============================================================  
+-- Propósito: Actualizar Tarjeta OH
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_cliente      -> Código de Cliente
+--    p_tipo_tarjeta    -> Tipo de Tarjeta
+--    p_num_tarjeta     -> Número de Tarjeta
+-- ===============================================================  
+Begin
+	Update tarjetaoh
+	   Set fecha_grabacion = current_timestamp, tipo_tarjeta = P_Tipo_Tarjeta -- , est_tarj = 1
+	Where numero_tarjeta = P_Num_Tarjeta;
+
+	Update solicitud_afiliacion
+	   Set estado_solicitud = 'Aprobado', fecha_grabacion = current_timestamp
+	Where numero_identidad = p_Id_Cliente;
+End&&
+delimiter ;
+
+DROP PROCEDURE IF EXISTS Solicitud_DesaprobadaI03;
+delimiter &&
+CREATE PROCEDURE Solicitud_DesaprobadaI03 (IN P_Id_Cliente INTEGER)
+-- ===============================================================  
+-- Propósito: Actualizar Solicitud Afiliacion
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_id_cliente      -> Código de Cliente
+-- ===============================================================  
+Begin
+	Update solicitud_afiliacion
+	  Set estado_solicitud ='Desaprobado', fecha_grabacion = current_timestamp
+	Where numero_identidad = p_id_cliente;
+End&&
+delimiter ;
+
+DROP PROCEDURE IF EXISTS Estado_Solicitud;
+delimiter &&
+CREATE PROCEDURE Estado_Solicitud (IN P_Num_Docu_Iden VARCHAR(10))
+-- ===============================================================  
+-- Propósito: Buscar Solicitud por Estado
+-- Autor: Jeison Juarez		Fecha: 01/06/2018
+-- ---------------------------------------------------------------
+-- Datos Relevantes:
+--    p_num_docu_iden     -> Número de Documento de Identidad
+-- ===============================================================  
+Begin
+	Select estado_solicitud
+	From solicitud_afiliacion
+	Where numero_identidad = p_Num_Docu_Iden;
+End&&
+delimiter ;
